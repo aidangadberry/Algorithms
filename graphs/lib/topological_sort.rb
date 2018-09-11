79cfa3a15e4ebacf0f58814412e2b408157256bf
Aidan Gadberry <agadberr@gmail.com>
2018-09-10 20:27:27 -0700

    Add topological sort Khan

--- graphs/lib/topological_sort.rb
+++ graphs/lib/topological_sort.rb
@@ -1,7 +1,33 @@
 require_relative 'graph'
 
-# Implementing topological sort using both Khan's and Tarian's algorithms
+# Implementing topological sort using both Khan's and Tarjan's algorithms
+
+# Khan's
 
 def topological_sort(vertices)
+  queue = []
+  order = []
 
+  vertices.each do |vertex|
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    curr = queue.shift
+    neighbors = []
+    curr.out_edges.each {|edge| neighbors << edge.to_vertex }
+
+    curr.out_edges.each {|edge| edge.destroy!}
+
+    neighbors.each do |adj|
+      queue << adj unless queue.include?(adj)
+    end
+
+    vertices.delete(curr)
+    order << curr
+  end
+
+  return [] unless order.uniq.length == order.length
+  order
 end
+
